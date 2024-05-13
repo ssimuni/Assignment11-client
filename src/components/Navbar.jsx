775eed28a823097ed5ce5e7af5e49c41eda0ed49
unknown <ssimuni>
2024-05-13 21:15:27 +0600

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = () => {
 
     const renderLoggedOutNavbar = () => {
         return (
-            <div>
+            <div className='flex'>
                 <Link to="/login">
                     <button className="btn ml-5 text-white" style={gradientBackground}>Log In</button>
                 </Link>
@@ -168,7 +168,7 @@ const Navbar = () => {
 
             <div className="navbar-end">
 
-                <label className="flex cursor-pointer gap-2">
+                <label className="lg:flex hidden cursor-pointer gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
                     <input type="checkbox" value="synthwave" className="toggle theme-controller" onChange={handleToggle} />
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
